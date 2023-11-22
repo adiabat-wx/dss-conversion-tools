5e557894513e7eccf1ade3bd92a4681d0cb3d23b
Ashley Ballard <104953582+vtwxash@users.noreply.github.com>
2023-11-22 09:04:04 -0500

    Update dss_to_tif.py
    adjust str concat

--- example/dss_to_tif.py
+++ example/dss_to_tif.py
@@ -2,6 +2,7 @@ from mil.army.usace.hec.vortex.io import TiffDataWriter
 from mil.army.usace.hec.vortex.io import DataReader, DssDataReader
 
 # Specify the path to .dss file 
+OUT_P = 'C:/tif_files'
 dss_fp = 'C:/dss_files/precip_20180505.dss'
 
 # Get dss pathnames
@@ -10,13 +11,13 @@ sourceGrids = DataReader.getVariables(dss_fp)
 # Loop through variables in dss file
 for vName in sourceGrids:
 
-    # Grab metadata for raster filename
+    # Grab metadata for raster filename (may need adjusting based on format of dss)
     meta = [str(i) for i in vName.split('/')]
     start = meta[4].replace(':','H')
     end = meta[5].replace(':','H')
 
-    # Specify output raster
-    tif_fp = 'C:/pythonscripts/merced/precip_obs_'+start+'_'+end+'.tif'
+    # Specify output raster (example for obs only dss file)
+    tif_fp = '{}/precip_obs_{}_{}.tif'.format(OUT_P, start, end)
         
     # Read DSS file to get variable as type <VortexData>
     srcs = DssDataReader.builder()\
